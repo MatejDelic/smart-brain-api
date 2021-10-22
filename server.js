861eab6e66450fcf12bc8daabe2ea0c0d927469d
MatejDelic <62113655+MatejDelic@users.noreply.github.com>
2021-10-22 20:47:12 +0200

    Add files via upload

---
+++ server.js
@@ -0,0 +1,109 @@
+const express = require('express');
+const bcrypt = require('bcrypt-nodejs');
+var cors = require('cors');
+const knex = require('knex');
+
+const db = knex({
+  client: 'pg',
+  connection: {
+    host : '127.0.0.1',
+    user : 'postgres',
+    password : 'mojasifrazasql',
+    database : 'smart-brain'
+  }
+});
+
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+
+app.get('/', (req, res) => {
+	res.send('success');
+})
+
+app.post('/signin', (req, res) => {
+	db.select('email', 'hash').from('login')
+		.where('email', '=', req.body.email)
+		.then(data => {
+			const isValid = bcrypt.compareSync(req.body.password, data[0].hash);
+			if (isValid) {
+				return db.select('*').from('users')
+					.where('email', '=', req.body.email)
+					.then(user => {
+						res.json(user[0])
+					})
+					.catch(err => res.status(400).json('unable to get user'))
+			} else {
+				res.status(400).json('wrong credentials')
+			}
+		})
+		.catch(err => res.status(400).json('wrong credentials'))
+})
+
+app.post('/register', (req, res) => {
+	const { email, name, password } = req.body;
+	const hash = bcrypt.hashSync(password);
+	db.transaction(trx => {
+		trx.insert({
+			hash: hash,
+			email: email
+		})
+		.into('login')
+		.returning('email')
+		.then(loginEmail => {
+			return trx('users')
+				.returning('*')
+				.insert({
+					email: loginEmail[0],
+					name: name,
+					joined: new Date()
+				})
+				.then(user => {
+					res.json(user[0]);							/*ovo [0] je samo da smo sigurni da ne vraca array nego objekt, ali unosimo samo jednog */
+				})												/*usera pa bi svakako triba bit objekt */
+		})
+		.then(trx.commit)
+		.catch(trx.rollback)			/*ako nesto ne uspije, ne sprema nikakve promjene*/
+	})
+	.catch(err => res.status(400).json('unable to register'))
+})
+
+app.get('/profile/:id', (req, res) => {
+	const { id } = req.params;
+	let found = false;
+	db.select('*').from('users').where({id})			/*{id} je isto sta i {id: id}, ovo mozemo kad su oba ista naziva u objektu*/
+		.then(user => {
+			if (user.length) {
+				res.json(user[0])
+			} else {
+				res.status(400).json('Not found')
+			}
+		})
+		.catch(err => res.status(400).json('error getting user'))
+})
+
+app.put('/image', (req, res) => {
+	const { id } = req.body;
+	db('users').where('id', '=', id)
+	.increment('entries', 1)
+	.returning('entries')
+	.then(entries => {
+		res.json(entries[0]);
+	})
+	.catch(err => res.status(400).json('unable to get entries'))
+})
+
+app.listen(3001, () => {
+	console.log('app is running on port 3001');
+})
+
+/*
+
+/ --> res = this is working
+/signin --> POST = succes/fail
+/register --> POST = user
+/profile/:userId --> GET = user
+/image --> PUT = user
+
+*/
